7c2f36907b630828c4c3dfc28d7c2a90777cfe4d
dianabeta <dianabetancourt7@gmail.com>
2020-08-07 15:00:22 +0200

    finished gallery

--- gallery.js
+++ gallery.js
@@ -7,21 +7,17 @@ function upDate(previewPic){
 
     2) Change the text  of the div with the id = "image"
     to the alt text of the preview image
+
+"url (" + str(previewPic.src) + ")"
     */
 
-    document.getElementById("image").style.backgroundImage="url (" +previewPic.src + ")" ;
 
-
+    document.getElementById('image').style.backgroundImage="url('"+previewPic.src+"')";
+    document.getElementById('image').innerHTML= previewPic.alt;
   }
-	function unDo(){
 
-    document.getElementById('image').innerHTML='Hover over an image';
-     /* In this function you should
-    1) Update the url for the background image of the div with the id = "image"
-    back to the orginal-image.  You can use the css code to see what that original URL was
+function unDo(){
 
-    2) Change the text  of the div with the id = "image"
-    back to the original text.  You can use the html code to see what that original text was
-    */
-
-	}
+    document.getElementById('image').style.backgroundImage= "url( )";
+    document.getElementById('image').innerHTML= "Hover an image bellow to display here";
+}
